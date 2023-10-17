10801432d7e5cf6941016c36b18db2c87aead594
betül akbulut <betulakbulut8@gmail.com>
2023-10-17 13:47:47 +0300

    kart eslestirme oyunu

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "“Sinekli Bakkal” Romanının Yazarı Aşağıdakilerden Hangisidir?",
+        answer: "Halide Edip Adıvar",
+        options: [
+            "Halide Edip Adıvar",
+            "Reşat Nuri Güntekin",
+            "Ziya Gökalp",
+            "Ömer Seyfettin"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Raskolnikov hangi romanın kahramanıdır?",
+        answer: "Suç ve Ceza",
+        options: [
+            "Ölü Canlar",
+            "Suç ve Ceza",
+            "Kumarbaz",
+            "Sefiller"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Cevdet Bey ve Oğulları Eseri Kime Aittir?",
+        answer: "Orhan Pamuk",
+        options: [
+            "Orhan Pamuk",
+            "Yahya Kemal Bayatlı",
+            "Atilla İlhan",
+            "Samipaşazade"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Gulliver'in Gezileri adlı eserde minik insanların ülkesi hangisidirr?",
+        answer: "Lilliput",
+        options: [
+            "Yahoo",
+            "Brobdingna",
+            "Lilliput"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Kürk Mantolu Madonna adlı eser hangi yılda yayımlanmıştı?",
+        answer: "1943",
+        options: [
+            "1943",
+            "1945",
+            "1967",
+            "1970",
+            "1960"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+];
